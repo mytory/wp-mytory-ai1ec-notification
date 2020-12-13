2a2253779184687a626468c6a05e894e9ea33f74
mytory <mytory@gmail.com>
2020-12-14 02:32:34 +0900

    알림 시각을 조건에 따라 조정함. 알림 가능 불가능 여부도 일정 경과 여부에 따라 판단.

--- src/Ai1ecNotification.php
+++ src/Ai1ecNotification.php
@@ -4,6 +4,7 @@ namespace Mytory\Ai1ecNotification;
 
 use Ai1ec_Date_Time;
 use Ai1ec_Event;
+use Carbon\Carbon;
 use FCMSimple;
 use WP_Error;
 use WP_Post;
@@ -44,14 +45,39 @@ class Ai1ecNotification {
 		global $ai1ec_front_controller;
 
 		date_default_timezone_set( wp_timezone()->getName() );
-		$ai1ec_registry             = $ai1ec_front_controller->return_registry( true );
-		$event                      = $ai1ec_registry->get( 'model.event', $post_id );
-		$start                      = $event->get( 'start' );
-		$start_datetime_for_compare = ( ( $event->is_allday() ) ? $start->format( 'Y-m-d 09:00:00' ) : $start->format( 'Y-m-d H:i:s' ) );
-		$notification_time          = strtotime( $start_datetime_for_compare ) - ( 60 * 60 );
+		$ai1ec_registry = $ai1ec_front_controller->return_registry( true );
+		$event          = $ai1ec_registry->get( 'model.event', $post_id );
+		$start_carbon   = Carbon::createFromFormat( 'Y-m-d H:i:s', $event->get( 'start' )->format( 'Y-m-d H:i:s' ), wp_timezone()->getName() );
+
+		// 기본 알림 시각은 시작 시각 1시간 전
+		$notification_time = $start_carbon->clone()->subHour();
+		$now               = Carbon::now();
+
+		if ( $event->is_allday() and $start_carbon->isSameDay( $now ) ) {
+			// 하루종일 일정이고 같은 날이면 바로 보낸다.
+			$notification_time = $now;
+		}
+
+		if ( $event->is_allday() and $start_carbon->gt( $now ) ) {
+			// 하루종일 일정이고 지금보다 뒤면 당일 오전 8시에 알림을 보내다.
+			$notification_time = $start_carbon->clone()->setHour( 8 )->setMinute( 0 )->setSecond( 0 );
+		}
+
+		if ( ! $event->is_allday() and $start_carbon->gt( $now ) and $notification_time->lt( $now ) ) {
+			// 하루종일 일정이 아니고 시작 시각이 현재 시각보다 뒤지만, 알림 시각은 지난 경우라면 바로 알림을 보낸다.
+			$notification_time = $now;
+		}
+
+		$response = self::canPushNotification( $event->is_allday(), $start_carbon );
+		if ( ! $response['result'] ) {
+			return;
+		}
+
+		update_post_meta( $post_id, '_ai1ec_push_notified', '예약' );
+		update_post_meta( $post_id, '_ai1ec_push_reserved_time', $notification_time->format( 'U' ) );
 
 		// 일정 시작 한 시간 전으로 예약을 건다. 이렇게 하면 알림 시작 시각이 지났으면 바로 알림이 가게 된다.
-		wp_schedule_single_event( $notification_time, 'sendPush', [ $post_id ] );
+		wp_schedule_single_event( $notification_time->format( 'U' ), 'sendPush', [ $post_id ] );
 	}
 
 	/**
@@ -62,10 +88,7 @@ class Ai1ecNotification {
 	 * @return bool
 	 */
 	private function needToPush( WP_Post $post ) {
-		if ( ! defined( 'FCM_TOKEN' ) ) {
-			return false;
-		}
-		if ( empty( FCM_TOKEN ) ) {
+		if ( ! defined( 'FCM_TOKEN' ) or empty( FCM_TOKEN ) ) {
 			return false;
 		}
 		if ( $post->post_status != 'publish' ) {
@@ -74,12 +97,54 @@ class Ai1ecNotification {
 		if ( empty( $_POST['ai1ec_notification']['medium'] ) ) {
 			return false;
 		}
-		if ( $_POST['ai1ec_notification']['medium'] != 'push' ) {
+		if ( ! in_array( 'push', $_POST['ai1ec_notification']['medium'] ) ) {
 			return false;
 		}
 
-
 		return true;
+	}
+
+	/**
+	 * @param bool $is_allday
+	 * @param Carbon $start_carbon
+	 *
+	 * @return array [ 'result': 결과. true/false, 'message': result가 false인 경우 이유. ]
+	 */
+	public static function canPushNotification( bool $is_allday, Carbon $start_carbon ): array {
+		date_default_timezone_set( wp_timezone()->getName() );
+
+		$now = Carbon::now();
+
+		if ( $is_allday and $start_carbon->format( 'Y-m-d' ) < $now->format( 'Y-m-d' ) ) {
+			return [
+				'result'  => false,
+				'message' => '날짜가 지났습니다.',
+			];
+		}
+
+		if ( ! $is_allday and $start_carbon->lt( $now ) ) {
+			return [
+				'result'  => false,
+				'message' => '일정 시작 시각이 지났습니다.',
+			];
+		}
+
+		$push_notified = get_post_meta( get_the_ID(), '_ai1ec_push_notified', true );
+		if ( $push_notified == '예약' ) {
+			$push_reserved_time = get_post_meta( get_the_ID(), '_ai1ec_push_reserved_time', true );
+			$reserved_carbon    = Carbon::createFromFormat( 'U', $push_reserved_time, wp_timezone()->getName() );
+			$reserved_carbon->setTimezone( wp_timezone()->getName() );
+
+			return [
+				'result'  => false,
+				'message' => $reserved_carbon->format( 'n월 j일 G시 i분 발송으로 예약되었습니다.' ),
+			];
+		}
+
+		return [
+			'result'  => true,
+			'message' => '',
+		];
 	}
 
 	public function kakaoSaveHook( $post_id, $post, $is_update ) {
@@ -93,30 +158,49 @@ class Ai1ecNotification {
 		global $ai1ec_front_controller;
 
 		date_default_timezone_set( wp_timezone()->getName() );
-		$ai1ec_registry             = $ai1ec_front_controller->return_registry( true );
-		$event                      = $ai1ec_registry->get( 'model.event', $post_id );
-		$start                      = $event->get( 'start' );
-		$start_datetime_for_compare = ( ( $event->is_allday() ) ? $start->format( 'Y-m-d 09:00:00' ) : $start->format( 'Y-m-d H:i:s' ) );
-		$notification_time          = strtotime( $start_datetime_for_compare ) - ( 60 * 60 );
+		$ai1ec_registry = $ai1ec_front_controller->return_registry( true );
+		$event          = $ai1ec_registry->get( 'model.event', $post_id );
+		$start_carbon   = Carbon::createFromFormat( 'Y-m-d H:i:s', $event->get( 'start' )->format( 'Y-m-d H:i:s' ), wp_timezone()->getName() );
 
-		/*
-		 * 일정 시각에 알림을 보낸다.
-		 * 일정 시각이 오후 8시에서 오전 8시 사이라면 오후 7시 반에 알림을 보내는 게 좋을 것이다.
-		 */
-		$hour = $start->format( 'H' );
-		if ( $hour < 8 ) {
-			// 일정 시각이 자정에서 오전 8시 사이라면
-			$time = strtotime( date( 'Y-m-d 19:30:00', strtotime( '-1 day ' . $start_datetime_for_compare ) ) );
-		} elseif ( $hour >= 20 ) {
-			// 일정 시각이 오후 8시에서 자정 사이라면 보낼 시각은 직전인 오후 7시 반
-			$time = strtotime( date( 'Y-m-d 19:30:00', strtotime( $start_datetime_for_compare ) ) );
-		} else {
-			// 일정 시각이 오전 8시에서 오후 20시 사이라면 $notification_time에 보낸다.
-			$time = strtotime( $notification_time );
+		// 기본 알림 시각은 시작 시각 1시간 전
+		$notification_time = $start_carbon->clone()->subHour();
+		$now               = Carbon::now();
+
+		$response = self::canKakaoNotification( $event->is_allday(), $start_carbon );
+		if ( ! $response['result'] ) {
+			return;
 		}
+
+		if ( $event->is_allday() and $start_carbon->isSameDay( $now ) and $start_carbon->format( 'H' ) < 20 ) {
+			// 하루종일 일정이고 같은 날이고 오후 8시 전이면 바로 보낸다.
+			$notification_time = $now;
+		}
+
+		if ( $event->is_allday() and $start_carbon->gt( $now ) ) {
+			// 하루종일 일정이고 지금보다 뒤면 당일 오전 8시에 알림을 보내다.
+			$notification_time = $start_carbon->clone()->setHour( 8 )->setMinute( 0 )->setSecond( 0 );
+		}
+
+		if ( ! $event->is_allday() and $start_carbon->format('Y-m-d') > $now->format('Y-m-d') ) {
+			// 하루종일 일정이 아니고 시작 날짜가 내일 이후인 경우
+			if ($notification_time->format('H') >= 20) {
+				// 시작 시각 한 시간 전인 알림 시각이 오후 8시 이후가 된 경우에는 알림 시각을 강제로 오후 8시로 조정한다.
+				$notification_time->setHour(20)->setMinute(0)->setSecond(0);
+			} elseif ($notification_time->format('H') < 8) {
+				// 시작 시각 한 시간 전인 알림 시각이 오전 8시 이전인 경우에는 알림 시각을 강제로 오전 8시로 조정한다.
+				$notification_time->setHour(8)->setMinute(0)->setSecond(0);
+			}
+			// 그 밖의 경우에는 현재 알림 시각을 유지한다.
+		}
+
+		if ( ! $event->is_allday() and $start_carbon->gt( $now ) and $notification_time->lt( $now ) ) {
+			// 하루종일 일정이 아니고 시작 시각이 현재 시각보다 뒤지만, 알림 시각은 지난 경우라면 바로 알림을 보낸다.
+			$notification_time = $now;
+		}
+
 		update_post_meta( $post_id, '_ai1ec_kakao_notified', '예약' );
-		update_post_meta( $post_id, '_ai1ec_kakao_reserved_time', $time );
-		wp_schedule_single_event( $time, 'sendKakao', [ $post_id ] );
+		update_post_meta( $post_id, '_ai1ec_kakao_reserved_time', $notification_time->format( 'U' ) );
+		wp_schedule_single_event( $notification_time->format( 'U' ), 'sendKakao', [ $post_id ] );
 	}
 
 	/**
@@ -142,27 +226,106 @@ class Ai1ecNotification {
 		if ( empty( $_POST['ai1ec_notification']['medium'] ) ) {
 			return false;
 		}
-		if ( $_POST['ai1ec_notification']['medium'] != 'kakao' ) {
+		if ( ! in_array( 'kakao', $_POST['ai1ec_notification']['medium'] ) ) {
 			return false;
 		}
 
 		return true;
 	}
 
+	/**
+	 * @param bool $is_allday
+	 * @param Carbon $start_carbon
+	 *
+	 * @return array [ 'result': 결과. true/false, 'message': result가 false인 경우 이유. ]
+	 */
+	public static function canKakaoNotification( bool $is_allday, Carbon $start_carbon ): array {
+		date_default_timezone_set( wp_timezone()->getName() );
+
+		$now = Carbon::now();
+		if ( $is_allday and $start_carbon->format( 'Y-m-d' ) < $now->format( 'Y-m-d' ) ) {
+			return [
+				'result'  => false,
+				'message' => '날짜가 지났습니다.',
+			];
+		}
+
+		if ( $now->format( 'H' ) >= 20 and $start_carbon->isSameDay( $now ) ) {
+			return [
+				'result'  => false,
+				'message' => '카카오톡은 오후 8시 이후에 알림을 보낼 수 없습니다.',
+			];
+		}
+
+		if ( ! $is_allday and $start_carbon->lt( $now ) ) {
+			return [
+				'result'  => false,
+				'message' => '일정 시작 시각이 지났습니다.',
+			];
+		}
+
+		if ($start_carbon->gt($now) and $start_carbon->format('H') < 8 and $start_carbon->clone()->subDay()->setHour(20)->setMinute(0)->setSecond(0)->lt($now)) {
+			// 시작 시각이 현재보다 뒤고, 시작 시각이 오전 8시 전이고, 시작 시각 하루 전날 오후 8시가 이미 지났다면 알림을 보내지 않는다.
+			return [
+				'result'  => false,
+				'message' => '카카오톡은 오전 8시 전에 알림을 보낼 수 없습니다.',
+			];
+		}
+
+		$kakao_notified = get_post_meta( get_the_ID(), '_ai1ec_kakao_notified', true );
+		if ( $kakao_notified == '예약' ) {
+			$kakao_reserved_time = get_post_meta( get_the_ID(), '_ai1ec_kakao_reserved_time', true );
+			$reserved_carbon     = Carbon::createFromFormat( 'U', $kakao_reserved_time, wp_timezone()->getName() );
+			$reserved_carbon->setTimezone( wp_timezone()->getName() );
+
+			return [
+				'result'  => false,
+				'message' => $reserved_carbon->format( 'n월 j일 G시 i분 발송으로 예약되었습니다.' ),
+			];
+		}
+
+		return [
+			'result'  => true,
+			'message' => '',
+		];
+	}
+
 	public function sendKakao( $post_id ) {
 
 		$post = get_post( $post_id );
 
-		$contact_query = new WP_Query( [
-			'post_type'      => 'mytory_contact',
-			'posts_per_page' => - 1,
-			'post_status'    => 'any',
-		] );
+		$contacts = [];
+
+		if ( defined( 'NOTIFICATION_TEST' ) and NOTIFICATION_TEST and defined( 'FCM_TEST_TARGET' ) and FCM_TEST_TARGET ) {
+			// 테스트 모드
+			$contact_query = new WP_Query( [
+				'post_type'      => 'mytory_contact',
+				'posts_per_page' => - 1,
+				'post_status'    => 'any',
+				'meta_query'     => [
+					[
+						'key'   => 'phone',
+						'value' => '01044533153',
+					]
+				]
+			] );
+			$contacts      = $contact_query->posts;
+		}
+
+		if ( defined( 'NOTIFICATION_TEST' ) and NOTIFICATION_TEST === false ) {
+			// NOTIFICATION_TEST가 설정돼 있고 false여야 실제 알림을 보낸다.
+			$contact_query = new WP_Query( [
+				'post_type'      => 'mytory_contact',
+				'posts_per_page' => - 1,
+				'post_status'    => 'any',
+			] );
+			$contacts      = $contact_query->posts;
+		}
 
 		$success_count = 0;
 		$fail_count    = 0;
 
-		foreach ( $contact_query->posts as $contact ) {
+		foreach ( $contacts as $contact ) {
 			$body_php = [
 				'account' => KAKAO_ACCOUNT,
 				'refkey'  => 'koreamsc_' . $post_id,
@@ -234,16 +397,27 @@ class Ai1ecNotification {
 			'url'   => get_permalink( $post ),
 		);
 
-		$fcm      = new FCMSimple( FCM_TOKEN );
-		$response = $fcm->sendToTopic(
-			'/topics/notice',
-			$data,
-			[
-				'title' => $title,
-				'body'  => $data['body'],
-				'sound' => 'default',
-			]
-		);
+		$fcm = new FCMSimple( FCM_TOKEN );
+
+		if ( defined( 'NOTIFICATION_TEST' ) and NOTIFICATION_TEST and defined( 'FCM_TEST_TARGET' ) and FCM_TEST_TARGET ) {
+			// 테스트 모드
+			$fcm->setTokens( [ FCM_TEST_TARGET ] );
+			$fcm->sendToUsers( $data );
+		}
+
+		if ( defined( 'NOTIFICATION_TEST' ) and NOTIFICATION_TEST === false ) {
+			// NOTIFICATION_TEST가 false로 돼 있어야 실제 알림을 보낸다.
+			$response = $fcm->sendToTopic(
+				'/topics/notice',
+				$data,
+				[
+					'title' => $title,
+					'body'  => $data['body'],
+					'sound' => 'default',
+				]
+			);
+		}
+
 		update_post_meta( $post_id, '_ai1ec_push_notified', true );
 		update_post_meta( $post_id, '_ai1ec_push_datetime', 'YmdHis' );
 		update_post_meta( $post_id, '_ai1ec_push_response', $response );
--- src/metabox.php
+++ src/metabox.php
@@ -1,20 +1,39 @@
 <?php
+
+use Carbon\Carbon;
+use Mytory\Ai1ecNotification\Ai1ecNotification;
+
 $push_response       = json_decode( get_post_meta( get_the_ID(), '_ai1ec_push_response', true ), true );
 $push_notified       = get_post_meta( get_the_ID(), '_ai1ec_push_notified', true );
 $push_reserved_time  = get_post_meta( get_the_ID(), '_ai1ec_push_reserved_time', true );
 $kakao_result        = get_post_meta( get_the_ID(), '_ai1ec_kakao_result', true );
 $kakao_notified      = get_post_meta( get_the_ID(), '_ai1ec_kakao_notified', true );
 $kakao_reserved_time = get_post_meta( get_the_ID(), '_ai1ec_kakao_reserved_time', true );
-date_default_timezone_set( 'Asia/Seoul' );
+
+global $ai1ec_front_controller;
+
+try {
+	/**
+	 * @var Ai1ec_Event $event
+	 * @var Ai1ec_Date_Time $start
+	 */
+	$ai1ec_registry         = $ai1ec_front_controller->return_registry( true );
+	$event                  = $ai1ec_registry->get( 'model.event', get_the_ID() );
+	$start_carbon           = Carbon::createFromFormat( 'Y-m-d H:i:s', $event->get( 'start' )->format( 'Y-m-d H:i:s' ), wp_timezone()->getName() );
+	$can_kakao_notification = Ai1ecNotification::canKakaoNotification( $event->is_allday(), $start_carbon );
+	$can_push_notification  = Ai1ecNotification::canPushNotification( $event->is_allday(), $start_carbon );
+} catch ( \Exception $e ) {
+
+}
 ?>
 
 <p>
   <label style="margin-right: .5rem;">
-	  <?php if ( $push_notified == '예약' ) { ?>
+	  <?php if ( ! empty( $can_push_notification ) and ! $can_push_notification['result'] ) { ?>
         <input type="checkbox" disabled> 푸시 알림:
-		  <?php echo( ! empty( $push_reserved_time ) ? date( 'n월 j일 G시 발송으로 예약되었습니다.', $push_reserved_time ) : '알림 예약되었습니다.' ); ?>
+		  <?php echo $can_push_notification['message']; ?>
 	  <?php } else { ?>
-        <input type="checkbox" name="ai1ec_notification[medium]" value="push">
+        <input type="checkbox" name="ai1ec_notification[medium][]" value="push">
 		  <?php if ( $push_notified ) {
 			  if ( ! empty( $push_response['message_id'] ) ) {
 				  ?>
@@ -31,12 +50,14 @@ date_default_timezone_set( 'Asia/Seoul' );
 	  <?php } ?>
   </label>
 
-	<?php if ( $kakao_notified == '예약' ) { ?>
+	<?php
+	// disabled인 경우. 예약이 이미 된 경우, 시작 시간이 지난 경우.
+	if ( ! empty( $can_kakao_notification ) and ! $can_kakao_notification['result'] ) { ?>
       <input type="checkbox" disabled> 카카오:
-		<?php echo( $kakao_reserved_time ? date( 'n월 j일 G시 발송으로 예약되었습니다.', $kakao_reserved_time ) : '오전 8시로 알림 예약되었습니다.' ); ?>
+		<?php echo $can_kakao_notification['message']; ?>
 	<?php } else { ?>
       <label>
-        <input type="checkbox" name="ai1ec_notification[medium]" value="kakao">
+        <input type="checkbox" name="ai1ec_notification[medium][]" value="kakao">
 		  <?php if ( $kakao_notified == '1' ) { ?>
             카카오 <b>다시 보내기</b> <small>(<?php echo $kakao_result ?>)</small>
 		  <?php } else { ?>
